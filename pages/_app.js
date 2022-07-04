e97ccc558e89ba6dde1aea401a9b56e169964c96
Soyab Mostofa <soyab@pop-os.localdomain>
2022-07-04 11:16:33 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import Footer from '../components/widgets/Footer';
 import Header from '../components/widgets/Header';
 import '../styles/globals.css';
@@ -6,6 +7,11 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Woodmax Decor</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="best furniture for dream space" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
